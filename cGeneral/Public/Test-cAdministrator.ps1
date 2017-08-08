d237df6ece587a87ae5cf02200a2982f2fd7a69a
Jeroen Koopman <jkoopman@asp4all.nl>
2017-08-08 09:43:00 +0200

    Remove unneeded install files

--- cGeneral/Public/Test-cAdministrator.ps1
+++ cGeneral/Public/Test-cAdministrator.ps1
@@ -10,9 +10,11 @@
 .OUTPUTS
  $true or $false
 #>
-function Test-cAdministrator  
-{  
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+function Test-cAdministrator {  
+    $CurUser = [Security.Principal.WindowsIdentity]::GetCurrent();
+    [boolean]$Privilege = (New-Object Security.Principal.WindowsPrincipal $CurUser).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+    if (!([boolean]$Privilege)) {
+        Write-Error "This script must be executed from an elevated PowerShell process" -Category PermissionDenied 
+    }
+    return $Privilege
 }
-Export-ModuleMember -Function Test-cAdministrator
